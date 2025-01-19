058ecb6c5ccc18542e0f85d3d086a5309850d093
hyejj19@naver.com <hyejj19@naver.com>
2025-01-20 00:43:06 +0900

    add: app bar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Movie flix',
+        ),
+      ),
+    );
+  }
+}
