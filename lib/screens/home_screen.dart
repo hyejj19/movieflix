d5c53c3d85bb82628e0794a63b48a493ad5d91fa
hyejj19@naver.com <hyejj19@naver.com>
2025-01-20 02:45:02 +0900

    connect all list api data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,20 +1,193 @@
 import 'package:flutter/material.dart';
 import 'package:movieflix/models/movie_model.dart';
 import 'package:movieflix/services/api_service.dart';
-import 'package:movieflix/widgets/popular_card_list.dart';
+import 'package:movieflix/widgets/popular_card.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
   final Future<List<MovieModel>> popularMovies = ApiService.getPopularMovies();
 
+  final Future<List<MovieModel>> nowInCinemas = ApiService.getNowInCinemas();
+
+  final Future<List<MovieModel>> comingSoon = ApiService.getComingSoon();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: PopularCardList(popularMovies: popularMovies),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 250,
+              child: FutureBuilder(
+                future: popularMovies,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Popular Movies',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.start,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Expanded(child: makeListWithImg(snapshot))
+                        ],
+                      ),
+                    );
+                  }
+                  return const Center(
+                    heightFactor: 5,
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 320,
+              child: FutureBuilder(
+                future: nowInCinemas,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Now in Cinemas',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.start,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Expanded(child: makeListWithTitleAndImg(snapshot))
+                        ],
+                      ),
+                    );
+                  }
+                  return const Center(
+                    heightFactor: 5,
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 320,
+              child: FutureBuilder(
+                future: comingSoon,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Coming soon',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.start,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Expanded(child: makeListWithTitleAndImg(snapshot))
+                        ],
+                      ),
+                    );
+                  }
+                  return const Center(
+                    heightFactor: 5,
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  ListView makeListWithImg(AsyncSnapshot<List<MovieModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var movie = snapshot.data![index];
+        return PopularCard(backdropPath: movie.backdropPath, id: movie.id);
+      },
+      separatorBuilder: (context, index) => SizedBox(
+        width: 15,
+      ),
+    );
+  }
+
+  ListView makeListWithTitleAndImg(AsyncSnapshot<List<MovieModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var movie = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Image.network(
+                movie.backdropPath,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Flexible(
+              child: SizedBox(
+                width: 150,
+                child: Text(
+                  movie.title,
+                  softWrap: true,
+                  maxLines: 3,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
+            )
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => SizedBox(
+        width: 15,
       ),
     );
   }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -6,6 +6,8 @@ import 'package:movieflix/models/movie_model.dart';
 class ApiService {
   static const String baseUrl = 'https://movies-api.nomadcoders.workers.dev';
   static const String popular = 'popular';
+  static const String nowPlaying = 'now-playing';
+  static const String comingSoon = 'coming-soon';
 
   static Future<List<MovieModel>> getPopularMovies() async {
     List<MovieModel> popularMovieInstances = [];
@@ -18,6 +20,38 @@ class ApiService {
         popularMovieInstances.add(MovieModel.fromJson(movie));
       }
       return popularMovieInstances;
+    }
+
+    throw Error();
+  }
+
+  static Future<List<MovieModel>> getNowInCinemas() async {
+    List<MovieModel> nowInCenemasMovieInstances = [];
+    final url = Uri.parse('$baseUrl/$nowPlaying');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final movies = jsonDecode(response.body)['results'];
+      for (var movie in movies) {
+        nowInCenemasMovieInstances.add(MovieModel.fromJson(movie));
+      }
+      return nowInCenemasMovieInstances;
+    }
+
+    throw Error();
+  }
+
+  static Future<List<MovieModel>> getComingSoon() async {
+    List<MovieModel> comingSoonMovieInstances = [];
+    final url = Uri.parse('$baseUrl/$comingSoon');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final movies = jsonDecode(response.body)['results'];
+      for (var movie in movies) {
+        comingSoonMovieInstances.add(MovieModel.fromJson(movie));
+      }
+      return comingSoonMovieInstances;
     }
 
     throw Error();
